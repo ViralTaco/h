006f96f753979ab3f3286c889e40765ddc9dd88d
viraltaco <vt_@gmx.us>
2023-06-13 20:51:54 +0200

    First release

--- lib/h.h
+++ lib/h.h
@@ -1,45 +1,43 @@
 #ifndef com_viraltaco_h_h
-#define com_viraltaco_h_h "0.2.0"
+#define com_viraltaco_h_h "1.0.3"
 /// Copyright 2023 viraltaco_ <https://h.viraltaco.com>
 #include <numeric>
 
 namespace h::inline v1 {
  inline namespace details {
-  using std::size_t;
-  using hash_t = unsigned long long;
- } // namespace h::inline v1::inline details
- 
+   using std::size_t;
+   using hash_t = unsigned long long;
+ } // h::inline v1::inline details
+
  constexpr auto hash(char const* str, size_t len) noexcept -> hash_t {
-   return std::accumulate(str, str + len, hash_t{}, [](hash_t h, char8_t c) {
+   return std::accumulate(str, &str[len], hash_t{}, [](auto h, char8_t c) {
+     if (c == '-') { return h; }
+     
      h <<= 5;
-     if ('a' <= c and c <= 'z')
-       return h | (c - 'a');
-     else if ('A' <= c and c <= 'Z')
-       return h | (c - 'A');
-     else switch (c) {
-       case '\0':return h | 26; // 0b11010
-       case '=': return h | 27; // 0b11011
-       case '-': return h | 28; // 0b11100
-       case ',': return h | 29; // 0b11101
-       case ' ': return h | 30; // 0b11110
-       default:  return h | 31; // 0b11111
+     switch (c) {
+     case '\0':return h | 0b11010; // h bit_or 26
+     case '=': return h | 0b11011; // h bit_or 27
+     case ',': return h | 0b11101; // h bit_or 29
+     case ' ': return h | 0b11110; // h bit_or 30
+     default:
+      if ('a' <= c and c <= 'z') {
+        return h | (c - 'a');
+      } else if ('A' <= c and c <= 'Z') {
+        return h | (c - 'A');
+      } else {
+        return h | 0b11111; // h bit_or 31;
+      }
      }
-   }) - (3 * len);
+   });
  }
  
  template <class CharT, size_t N>
- consteval auto hash(CharT const(&str)[N]) {
-   return h::hash(str, N - sizeof '\0');
- }
+ consteval auto hash(CharT const(&str)[N]) { return h::hash(str, N - 1u); }
  
  inline namespace literals {
-  consteval auto operator""_h(char const* str, size_t len) {
-    return h::hash(str, len);
-  }
- 
-  static_assert ("hello"_h != "Hoi"_h);
-  static_assert ("hello"_h == "HELLO"_h);
-  static_assert ("hello"_h == h::hash("hello"));
- }  // namespace h::inline v1::inline literals
-}  // namespace h::inline v1
+   consteval auto operator""_h(char const* str, size_t len) {
+     return h::hash(str, len);
+   }
+ } // h::inline v1::inline literals
+} // h::inline v1
 #endif // com_viraltaco_h_h
--- tst/test.cxx
+++ tst/test.cxx
@@ -5,18 +5,23 @@ auto main() -> int {
   using h::literals::operator ""_h;
   using fmt::print, fmt::arg;
 #define PRINT_h(STR)                                                           \
-    print("\"{}\"_h{x:^{w}}{h:#x}\n{x:^33}{h:>64b}\n", STR,                    \
+  print("\"{}\"_h{x:^{w}}{h:#x}\n{x:^33}{h:>64b}\n", STR,                      \
     arg("x", ' '), arg("w", 30u - sizeof STR), arg("h", STR##_h));             \
-    static_assert (__LINE__|';')
-
+  static_assert (__LINE__, "Semicolon Required.")
+  
   PRINT_h("Hello");
   PRINT_h("HELLO");
   PRINT_h("Hi");
   PRINT_h("Lorem Ipsum Dolor");
+  PRINT_h("help");
+  PRINT_h("-help");
   PRINT_h("--help");
   PRINT_h("--help license");
   PRINT_h("--license");
   PRINT_h("--");
   PRINT_h("Hello, World!");
 #undef PRINT_h
+  static_assert ("hello"_h != "Hoi"_h);
+  static_assert ("hello"_h == "HELLO"_h);
+  static_assert ("hello"_h == h::hash("hello"));
 }

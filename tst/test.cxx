311e589f3035c4427c26a2a23840dafd115c7e40
Anthony Capobianco <liq4tdot@duck.com>
2026-01-09 16:55:09 +0100

    MOD: Added parens surrounding function like macro `PRINT_h(STR)` argument `STR`

--- tst/test.cxx
+++ tst/test.cxx
@@ -5,8 +5,8 @@ auto main() -> int {
   using h::literals::operator ""_h;
   using fmt::print, fmt::arg;
 #define PRINT_h(STR)                                                           \
-  print("\"{}\"_h{x:^{w}}{h:#x}\n{x:^33}{h:>64b}\n", STR,                      \
-    arg("x", ' '), arg("w", 30u - sizeof STR), arg("h", STR##_h));             \
+  print("\"{}\"_h{x:^{w}}{h:#x}\n{x:^33}{h:>64b}\n", (STR),                    \
+    arg("x", ' '), arg("w", 30u - sizeof (STR)), arg("h", STR##_h));           \
   static_assert (__LINE__, "Semicolon Required.")
   
   PRINT_h("Hello");

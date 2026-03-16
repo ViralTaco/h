4b64be340a9f89c1bb57a308fdebaba0fb7c9c4c
Anthony Capobianco <liq4tdot@duck.com>
2026-03-16 23:15:02 +0100

    WS

--- examples/options.cxx
+++ examples/options.cxx
@@ -5,18 +5,18 @@ int main(int argc, char **argv) {
   if (argc < 2) return EXIT_FAILURE;
 
   switch (auto arg = argv[1]; h::hash(arg)) {
-    using std::println;
-    using namespace h::literals;
-  case "--version"_h:
-  case "-v"_h:
-    println("{}", com_viraltaco_h_h);
-    break;
-  case "--help"_h:
-  case "-h"_h:
-    println("--help");
-    break;
-  default:
-    println("Invalid input: '{}'", arg);
-    return EXIT_FAILURE;
+      using std::println;
+      using namespace h::literals;
+    case "--version"_h:
+    case "-v"_h:
+      println("{}", com_viraltaco_h_h);
+      break;
+    case "--help"_h:
+    case "-h"_h:
+      println("--help");
+      break;
+    default:
+      println("Invalid input: '{}'", arg);
+      return EXIT_FAILURE;
   }
 }
